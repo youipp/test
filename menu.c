fdff019bbd970ee0a9a6445a50656d3b15c8819c
ParkUiSeong <dmltjd1008@handong.ac.kr>
2024-03-29 20:27:10 +0900

    any

--- menu.c
+++ menu.c
@@ -1,9 +1,46 @@
 //menu.c
 #include "menu.h"
 
+#define SIZE 50
+struct st_channel* clist[SIZE]; // channel list
+
 void displayMenu(){
-    printf("*****************\n");
-    printf("1. Pizza : 20000 \n");
-    printf("2. Spaghetti: 12000\n");
-    printf("*****************\n");
+    srand(time(0));
+    int no;	// amount of channels
+    int menu; // menu number 
+    no = loadData(clist);
+    while(1)
+	{
+		// Print menu
+		printf("\n[1]List [2]Statistics [3]Random pick [4]Search [5]Add [6]Modify [7]Delete [8]Report [0]Exit\n> Enter a menu >> ");
+		scanf("%d",&menu);	
+
+		if(menu==1){
+			printChannels(clist, no); // Print all list of channels
+		}
+		else if(menu==2){
+			printStatistics(clist, no);	// Print statistics of each level
+		}
+		else if(menu==3){
+			pickupRandomChannels(clist, no); // Pick up random channels
+		}
+		else if(menu==4){
+			searchChannel(clist, no);	// Search a channel
+		}
+		else if(menu==5){
+			no = addChannel(clist, no); // Add a channel
+		}
+		else if(menu==6){
+			updateChannel(clist, no); // Modify a channel
+		}
+		else if(menu==7){
+			no = deleteChannel(clist, no); // Delete a channel
+		}
+		else if(menu==8){
+			makeReport(clist, no);	// Make a Report
+		}
+		else {
+			break;
+		}
+	}
 }
