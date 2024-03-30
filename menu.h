d51d5dd6ed6e5aadb07c36250f303d4e981598c5
ParkUiSeong <dmltjd1008@handong.ac.kr>
2024-03-30 13:40:19 +0900

    comp

--- menu.h
+++ menu.h
@@ -3,20 +3,20 @@
 #include <string.h>
 
 struct st_Book{
-	char name[100]; // Channel name
-	int count;	  // # of subscriptions
+	char name[100]; // Book name
+	int count;	  // # of Words
 	int level;	  // index of level (0~4)
 };
 
 void displayMenu();
 
-int loadData(struct st_channel* c[]); 
+int loadData(struct st_Book* c[]); 
 int findLevel(int num); 
-void printStatistics(struct st_channel* c[], int size);
-void printBook(struct st_channel* c[], int size);
-void pickupRandomBooks(struct st_channel* c[], int size);
-void searchBook(struct st_channel* c[], int size);
-int addBook(struct st_channel* c[], int size);
-void updateBook(struct st_channel* c[], int size);
-int deleteBook(struct st_channel* c[], int size);
-void makeReport(struct st_channel* c[], int size); 
+void printStatistics(struct st_Book* c[], int size);
+void printBooks(struct st_Book* c[], int size);
+void pickupRandomBooks(struct st_Book* c[], int size);
+void searchBook(struct st_Book* c[], int size);
+int addBook(struct st_Book* c[], int size);
+void updateBook(struct st_Book* c[], int size);
+int deleteBook(struct st_Book* c[], int size);
+void makeReport(struct st_Book* c[], int size); 
